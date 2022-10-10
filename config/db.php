7372bafc14ee938b6e866b4dbce573b2662c36a8
koendebruijn <bruijnk@gmail.com>
2022-10-10 21:58:23 +0200

    Added db credentials

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=jbb8y3dri1ywovy2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=nf9oslm3qog1veyk',
+    'username' => 'hrkalljmpyw7ni60',
+    'password' => 'r7t4dx13cp727obm',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
